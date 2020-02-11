0c19c1ed0d35bbf7d4f512bd1337ad8b39854325
Brian Warner <warner@lothar.com>
2020-02-11 10:30:18 -0800

    build-kernel: stop using `__dirname`

--- packages/SwingSet/scripts/build-kernel.js
+++ packages/SwingSet/scripts/build-kernel.js
@@ -1,10 +1,14 @@
 import fs from 'fs';
+import path from 'path';
+import url from 'url';
 import process from 'process';
 import bundleSource from '@agoric/bundle-source';
+const filename = url.fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 async function main() {
   const { source, sourceMap } = await bundleSource(
-    `${__dirname}/../src/kernel/index.js`,
+    `${dirname}/../src/kernel/index.js`,
   );
   const actualSource = `export default ${source}\n${sourceMap}`;
   const f = await fs.promises.open('src/bundles/kernel', 'w', 0o644);
