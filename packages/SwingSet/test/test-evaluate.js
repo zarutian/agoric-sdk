19e5c8f3a76fd9e5eb7a36a88d0191de0dbd4968
Brian Warner <warner@lothar.com>
2020-03-14 16:10:23 -0700

    swingset/test-evaluate: new syntax

--- packages/SwingSet/test/test-evaluate.js
+++ packages/SwingSet/test/test-evaluate.js
@@ -1,17 +1,17 @@
 import { test } from 'tape';
-import evaluate from '@agoric/evaluate';
+import { evaluateProgram } from '@agoric/evaluate';
 
 test('basic', t => {
-  t.deepEqual(evaluate('1+2'), 3);
-  t.deepEqual(evaluate('(a,b) => a+b')(1, 2), 3);
-  t.deepEqual(evaluate('(function(a,b) { return a+b; })')(1, 2), 3);
+  t.deepEqual(evaluateProgram('1+2'), 3);
+  t.deepEqual(evaluateProgram('(a,b) => a+b')(1, 2), 3);
+  t.deepEqual(evaluateProgram('(function(a,b) { return a+b; })')(1, 2), 3);
   t.end();
 });
 
 test('endowments', t => {
-  t.deepEqual(evaluate('1+a', { a: 2 }), 3);
-  t.deepEqual(evaluate('(a,b) => a+b+c', { c: 3 })(1, 2), 6);
-  t.deepEqual(evaluate('(function(a,b) { return a+b+c; })', { c: 3 })(1, 2), 6);
-  t.deepEqual(evaluate('1+a+b', { a: 2, b: 3 }), 6);
+  t.deepEqual(evaluateProgram('1+a', { endowments: { a: 2 }}), 3);
+  t.deepEqual(evaluateProgram('(a,b) => a+b+c', { endowments: { c: 3 }})(1, 2), 6);
+  t.deepEqual(evaluateProgram('(function(a,b) { return a+b+c; })', { endowments: { c: 3 }})(1, 2), 6);
+  t.deepEqual(evaluateProgram('1+a+b', { endowments: { a: 2, b: 3 }}), 6);
   t.end();
 });
