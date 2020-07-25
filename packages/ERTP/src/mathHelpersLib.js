1d0cdd110f176e031120c7b28cdefdc25d53d97a
Zarutian <zarutian@gmail.com>
2020-07-25 08:53:25 +0000

    Update mathHelpersLib.js to add rectAreasMathHelpers
    note: I havent performed any testing yet.
    use at your own risk.

--- packages/ERTP/src/mathHelpersLib.js
+++ packages/ERTP/src/mathHelpersLib.js
@@ -2,12 +2,14 @@ import { importManager } from '@agoric/import-manager';
 import natMathHelpers from './mathHelpers/natMathHelpers';
 import strSetMathHelpers from './mathHelpers/strSetMathHelpers';
 import setMathHelpers from './mathHelpers/setMathHelpers';
+import rectAreasMathHelpers from './mathHelpers/rectAreasMathHelpers';
 
 const manager = importManager();
 const mathHelpersLib = manager.addExports({
   nat: natMathHelpers,
   strSet: strSetMathHelpers,
   set: setMathHelpers,
+  rectAreas: rectAreasMathHelpers
 });
 
 export default mathHelpersLib;
