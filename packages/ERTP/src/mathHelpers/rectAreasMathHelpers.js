3f67eb4bb7610e8543c056e2f0157b8abc82c447
Zarutian <zarutian@gmail.com>
2020-07-25 05:10:38 +0000

    . ArrayOf guard written from memory

--- packages/ERTP/src/mathHelpers/rectAreasMathHelpers.js
+++ packages/ERTP/src/mathHelpers/rectAreasMathHelpers.js
@@ -1,2 +1,17 @@
 // global harden
 import nat from "@agoric/nat";
+// -inlined from "@zarutian/ocaps/js/guards"-
+// (dont relie on this module specifier, quick to link-rot)
+const RecordOf = (template) => {
+};
+const ArrayOf = (perItemGuard) => {
+  return harden({
+    coerce: (specimen, ejector) => {
+      return Array.prototype.map.call(specimen, (item) => perItemGuard.coerce(item, ejector));
+    },
+    toString: () => {
+      return "«array guard of ".concat(perItemGuard.toString(), "»");
+    }
+  });
+};
+// -inline ends-
