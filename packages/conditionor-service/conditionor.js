a70f2d3d8d457c74202dd54eef0f3ae899537355
Zarutian <zarutian@gmail.com>
2020-07-26 05:17:02 +0000

    .

--- packages/conditionor-service/conditionor.js
+++ packages/conditionor-service/conditionor.js
@@ -2,7 +2,19 @@
 /* global harden */
 import { E } from "@agoric/eventual-send";
 
-const makeConditionor = (timerService, zoeService) => {
+const makeConditionor = (timerService, zoeService, defaultInterval=300000) => {
+  const Cs = new Map();
+  const newC = (condition, callback) => {
+    const handle = harden({ cancel: () => {
+      try {
+        const { callback } = Cs.get(handle);
+        void E(callback).cancelled();
+      }
+      Cs.delete(handle);
+    } });
+    Cs.set(handle, { condition, callback });
+    return handle;
+  }
   const onTrue   = (condition, callback) => {};
   const onChange = (condition, callback) => {};
   return harden({ onTrue, onChange });
