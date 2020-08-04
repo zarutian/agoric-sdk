b8c1403138a2a2e0946216e80fde9c5f53c62491
Zarutian <zarutian@gmail.com>
2020-08-04 21:03:35 +0000

    .

--- packages/conditionor-service/conditionor.js
+++ packages/conditionor-service/conditionor.js
@@ -27,7 +27,14 @@ const jsonLogic = (() => {
     "cat": function() {
       return Array.prototype.join.call(arguments, "");
     },
-    
+    "substr": (source, start, end) => {
+      if (end < 0){
+        // JavaScript doesn't support negative end, this emulates PHP behavior
+        var temp = String(source).substr(start);
+        return temp.substr(0, temp.length + end);
+      }
+      return String(source).substr(start, end);
+    },
   };
   return jsonLogic;
 })();
