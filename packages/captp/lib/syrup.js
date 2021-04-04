924227a4be395f4053cebdaad23161f00a42fdc0
Zarutian <zarutian@gmail.com>
2021-04-04 02:14:37 +0000

    Update syrup.js
    Handle the bytestrings, strings, and symbols.

--- packages/captp/lib/syrup.js
+++ packages/captp/lib/syrup.js
@@ -2,10 +2,14 @@
 
 // See https://gitlab.com/spritely/syrup/-/blob/master/impls/racket/syrup/syrup.rkt
 
-const makeDecodingGetter = (eventualBytegetter) => {
+const makeDecodingGetter = (opt) => {
+  const { eventualBytegetter,
+          makeBytestring,
+          makeString,
+          makeSymbol, } = opt;
   const getter = async (self = getter) => {
-    const first = await eventualBytegetter();
-      var length = "";
+    const first = await eventualBytegetter(1n);
+      var length = 0n;
     switch (first) {
       case '1': // fallthrough
       case '2': // fallthrough
@@ -16,9 +20,33 @@ const makeDecodingGetter = (eventualBytegetter) => {
       case '7': // fallthrough
       case '8': // fallthrough
       case '9':
-        length = length.concat(first);
+        length = (length * 10n) + BigInt(Number.parse(first, 10));
         while (true) {
-          const byte = await eventualBytegetter();
+          const byte = await eventualBytegetter(1n);
+          switch (byte) {
+            case '0': // fallthrough -start-
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8': // fallthrough -end-
+            case '9':
+              length = (length * 10n) + BigInt(Number.parse(first, 10));
+              break; // end of case
+            default:
+              const payload = await eventualBytegetter(length);
+              switch (byte) {
+                case ':': return makeBytestring(payload);
+                case '"': return makeString(payload);
+                case "'": return makeSymbol(payload);
+                default:
+                  throw new Error("syrup decoding error #1");
+              }
+              break; // end of default case
+          }
         }
         break; // end the case
     }
