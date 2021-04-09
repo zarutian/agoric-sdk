e9e7d73fdaa1ae59f16e128feab668117cf53c36
Zarutian <zarutian@gmail.com>
2021-04-09 02:00:02 +0000

    Update captp_goblins.js
    .

--- packages/captp/lib/captp_goblins.js
+++ packages/captp/lib/captp_goblins.js
@@ -4,3 +4,15 @@ import { E, HandledPromise } from '@agoric/eventual-send';
 import { isPromise } from '@agoric/promise-kit';
 
 export { E };
+
+/**
+ * Create a CapTP connection.
+ *
+ * @param {string} ourId our name for the current side
+ * @param {(obj: Record<string, any>) => void} rawSend send a JSONable packet
+ * @param {any} bootstrapObj the object to export to the other side
+ * @param {Partial<CapTPOptions>} opts options to the connection
+ */
+export function makeCapTP(ourId, rawSend, bootstrapObj = undefined, opts = {}) {
+
+}
