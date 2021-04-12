7be8bcbe6ddc72a5c49fe1e658f109ea4619e45a
Zarutian <zarutian@gmail.com>
2021-04-12 01:56:59 +0000

    Update bimap.js
    .

--- packages/captp/lib/bimap.js
+++ packages/captp/lib/bimap.js
@@ -1,10 +1,10 @@
 /* global harden */
 /* global Map */
+/* global WeakMap */
 
 const BiMap => (iterable = [], self) => {
   const key2val = new Map(iterable);
   const val2key = new Map((new Array(iterable)).map(([key, val]) => [val, key]));
-  
   const realSelf = harden({
     get [Symbol.species]() { return BiMap },
     get size() { return key2val.size; },
