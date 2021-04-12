478dd9da1df62d675ab54befa7a993b50a67ae19
Zarutian <zarutian@gmail.com>
2021-04-12 03:31:10 +0000

    Update bimap.js
    Og lokið við þessar gagnabyggingar

--- packages/captp/lib/bimap.js
+++ packages/captp/lib/bimap.js
@@ -153,7 +153,63 @@ const WeakBiMap = (iterable, opts = {}, self) => {
   const key2val = new WeakMap(iterable);
   const val2key =     WeakValueFinalizingMap((new Array(iterable)).map(([k, v]) => [v, k]), opts );
   const realSelf = harden({
-    
+    get [Symbol.species]() { return WeakBiMap; },
+    get size() { return val2key.size; },
+    clear() {
+      key2val.clear();
+      val2key.clear();
+      return undefined;
+    },
+    delete(key) {
+      const had = key2val.has(key);
+      const val = key2val.get(key);
+      key2val.delete(key);
+      val2key.delete(val);
+      return had;
+    },
+    get(key) { return key2val.get(key); },
+    getByValue(val) { return val2key.get(val); },
+    has(key) { return key2val.has(key); },
+    hasByValue(val) { return val2key.has(val); },
+    set(key, val) {
+      key2val.set(key, val);
+      val2key.set(val, key);
+      return realSelf;
+    },
+    [Symbol.iterator]() {
+      const myIterator = val2key[Symbol.iterator]();
+      return harden({
+        next() {
+          const { done, value } = myIterator.next();
+          const [gildi, lykill] = value;
+          return harden({ done, value: [lykill, gildi] });
+        }
+      });
+    },
+    keys() {
+      const myIterator = realSelf[Symbol.iterator]();
+      return harden({
+        next() {
+          const { done, value } = myIterator.next();
+          const [lykill, gildi] = value;
+          return harden({ done, value: lykill });
+        },
+      });
+    },
+    values() {
+      const myIterator = realSelf[Symbol.iterator]();
+      return harden({
+        next() {
+          const { done, value } = myIterator.next();
+          const [lykill, gildi] = value;
+          return harden({ done, value: gildi });
+        },
+      });
+    },
+    entries() { return realSelf[Symbol.iterator](); },
+    forEach(callback, thisValue) {
+      return key2val.forEach((val, key, map) => callback.call(thisValue, val, key, realSelf));
+    },
   });
   return realSelf;
 };
